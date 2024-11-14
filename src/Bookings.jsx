43e57d9dae80004faa2ea7adcbca8e2de81990b6
charles de Lalaing <71701904+CharlesdeLalaing@users.noreply.github.com>
2024-11-14 14:23:37 +0100

    width adapted

--- src/Bookings.jsx
+++ src/Bookings.jsx
@@ -70,6 +70,8 @@ const BookingForm = () => {
   const [coupon, setCoupon] = useState("");
   const [appliedCoupon, setAppliedCoupon] = useState(null);
   const [couponError, setCouponError] = useState(null);
+  
+
 
 
   // Toggle contact section and close others if open
@@ -554,14 +556,14 @@ const renderPriceDetails = () => {
 
   const renderProgressBar = () => (
     <div className="flex justify-between items-center text-center mb-4">
-      <div className="w-4/5 h-2 bg-gray-300 rounded">
+      <div className="w-3/5 md:w-3/5 lg:w-4/5 h-2 bg-gray-300 rounded">
         <div
           className={`h-2 rounded ${
             currentStep === 1 ? "w-1/3" : currentStep === 2 ? "w-2/3" : "w-full"
           } bg-[#668E73]`}
         ></div>
       </div>
-      <span className="w-1/5 ml-2 text-sm text-[#668E73]">
+      <span className="w-2/5 md:w-2/5 lg:w-1/5 ml-2 text-sm text-[#668E73]">
         Étape {currentStep} sur 3
       </span>
     </div>
@@ -743,7 +745,7 @@ const renderPriceDetails = () => {
                 key={key}
                 type="button"
                 onClick={() => setSelectedCategory(key)}
-                className={`px-5 py-2 rounded-lg transition-all text-[16px] font-normal ${
+                className={`px-4 py-2 rounded-lg transition-all text-[14px] font-bolder ${
                   selectedCategory === key
                     ? "bg-[#668E73] text-white"
                     : "bg-[#668E73] bg-opacity-10 text-[#668E73] hover:bg-opacity-20"
@@ -753,7 +755,7 @@ const renderPriceDetails = () => {
               </button>
             ))}
           </div>
-          <div className="grid grid-cols-1 gap-6 md:grid-cols-2">
+          <div className="grid grid-cols-1 gap-6 md:grid-cols-1 lg:grid-cols-2">
             {extraCategories[selectedCategory].items.map((item) => (
               <div
                 key={item.id}
@@ -962,7 +964,7 @@ const renderPaymentForm = () => (
 );
 
   return (
-    <div className="p-6 mx-auto">
+    <div className="p-6 mx-auto w-full lg:w-[1024px] xl:w-[1440px]">
       <h1 className="mb-4 text-2xl font-bold">
         Réserver le Dôme des Libellules
       </h1>
@@ -976,7 +978,7 @@ const renderPaymentForm = () => (
         <form onSubmit={handleSubmit} className="mx-auto space-y-4">
           {/* Search Section */}
           <div className="border border-[#668E73] p-5 rounded space-y-4">
-            <div className="grid grid-cols-1 gap-4 text-left md:grid-cols-2 lg:grid-cols-5">
+            <div className="grid grid-cols-1 gap-4 text-left md:grid-cols-4 lg:grid-cols-5">
               {/* Arrival Date */}
               <div className="relative">
                 <label className="block text-[14px] md:text-[16px] font-medium text-[#9a9a9a] mb-1">
