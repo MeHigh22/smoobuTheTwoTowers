b4735bd8bf0945734302f79850218c369dd2940e
MeHigh22 <72247500+MeHigh22@users.noreply.github.com>
2024-11-28 11:40:47 +0100

    fixDepartureDate

--- src/components/booking/SearchSection.jsx
+++ src/components/booking/SearchSection.jsx
@@ -71,7 +71,7 @@ export const SearchSection = ({
               minDate={startDate || new Date()}
               dateFormat="dd/MM/yyyy"
               placeholderText="Sélectionnez une date"
-              className="w-full p-2 border border-gray-300 rounded focus:border-[#668E73] focus:ring-1 focus:ring-[#668E73]"
+              className="mt-1 block w-full rounded border-[#668E73] border text-[14px] md:text-[16px] shadow-sm focus:border-[#668E73] focus:ring-1 focus:ring-[#668E73] text-black bg-white h-12 p-2"
               isClearable={true}
               disabled={!startDate}
             />
